b3d544417537bf5bdbd63425e8992a0fd6379ab9
K.C. Hunter <kchunter@connectionseducation.com>
2016-04-08 16:28:35 -0400

    Adding tutorial part 3.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Accio Code Tutorials: D3js#
 
-*** Updated 3-28-16 ***
+*** Updated 4-8-16 ***
 
 This tutorial series for [Accio Code](http://www.youtube.com/user/CDPAdvertising "Accio Code on YouTube") is a beginner's course in D3.js, a JavaScript library for displaying data dynamically on a website.
 
--- js/d3jsTutorial.js
+++ js/d3jsTutorial.js
@@ -33,3 +33,31 @@ function svgExample()
         .attr("stroke-width", 3);
 
 }
+function visualizeOranges ()
+{
+    var orangeData  = [10, 30, 50, 100];
+
+    var canvas = d3.select(".orangeContainer")
+        .append("svg")
+        .attr("width", 768)
+        .attr("height", 1024);
+
+    var oranges     = canvas.selectAll("circle")
+        .data(orangeData)
+        .enter()
+        .append("circle")
+        .attr("fill", "orange")
+        .attr("cx", function (d,i)
+        {
+            return d + (i * 100);
+        })
+        .attr("cy", function (d)
+        {
+            return d;
+        })
+        .attr("r", function (d)
+        {
+            return d;
+        });
+
+}
