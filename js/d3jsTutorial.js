494e712b56c0f25328aa4b4a2082408c50dad77e
K.C. Hunter <kchunter@connectionseducation.com>
2016-04-08 12:39:21 -0400

    Video 2 files.

--- js/d3jsTutorial.js
+++ js/d3jsTutorial.js
@@ -7,19 +7,29 @@ function start ()
 
     console.log(d3);
 }
-/**
- * Created by KC on 4/8/2016.
- */
 function svgExample()
 {
-    var c = d3.select(".svgExample")
-            .append("svg")
-            .attr("width", 500)
-            .attr("height", 500);
+   var canvas = d3.select("body")
+       .append("svg")
+       .attr("width", 700)
+       .attr("height", 700);
 
-    var circle = c.append("circle")
+    var circle = canvas.append("circle")
         .attr("cx", 10)
         .attr("cy", 10)
-        .attr("radius", 10)
+        .attr("r", 10)
         .attr("fill", "blue");
+
+    var rectangle = canvas.append("rect")
+        .attr("width", 100)
+        .attr("height", 100);
+
+    var line = canvas.append("line")
+        .attr("x1", 0)
+        .attr("x2", 200)
+        .attr("y1", 100)
+        .attr("y2", 300)
+        .attr("stroke", "grey")
+        .attr("stroke-width", 3);
+
 }
